3af8a9b9715e3a7c25df76d90349cdb0edfd961c
Michal Cichra <michal@o2h.cz>
2014-05-05 11:46:35 +0200

    initial commit

---
+++ lib/rubymine/test.rb
@@ -0,0 +1,7 @@
+require "rubymine/test/version"
+
+module Rubymine
+  module Test
+    # Your code goes here...
+  end
+end
---
+++ lib/rubymine/test/version.rb
@@ -0,0 +1,5 @@
+module Rubymine
+  module Test
+    VERSION = "0.0.1"
+  end
+end
